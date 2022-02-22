68dbbcdcf9d53a0ca36af8c196617c9cc2971808
Eduardo Zamora <41426993+ednux1808@users.noreply.github.com>
2022-02-21 18:07:46 -0600

    Add files via upload

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>¿Quie es este Pokemon?</h1>
+
+  <!-- TODO: img-->
+  <pokemon-picture :pokemonId="156" :showPokemon="true" />
+
+  <!-- TODO: Opciones-->
+  <pokemon-options />
+
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions";
+import PokemonPicture from "@/components/PokemonPicture";
+
+export default {
+  name: "PokemonPage",
+  components:{
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
